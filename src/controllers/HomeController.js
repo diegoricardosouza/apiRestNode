ea06a4d69a406b5b6e7a3fba3af18531552d4903
Diego Ricardo <diegoricardoweb@gmail.com>
2020-10-16 23:38:44 -0300

    config and testing sequelize

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,9 +1,18 @@
 /* eslint linebreak-style: ["error", "windows"] */
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Diego',
+      sobrenome: 'Ricardo',
+      email: 'diegoricardoweb@gmail.com',
+      idade: 30,
+      peso: 100,
+      altura: 1.95,
     });
+
+    res.json(novoAluno);
   }
 }
 
