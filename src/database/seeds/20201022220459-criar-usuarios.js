3295542f3363d37fcf05850fbddba5977680b976
Diego Ricardo <diegoricardoweb@gmail.com>
2020-10-22 19:54:50 -0300

    sequelize db seeds users

---
+++ src/database/seeds/20201022220459-criar-usuarios.js
@@ -0,0 +1,35 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          nome: 'John Doe',
+          email: 'johndoe@email.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'John Doe 2',
+          email: 'johndoe2@email.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'John Doe 3',
+          email: 'johndoe3@email.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  down: async () => {},
+};
